d0f73c582760ad6240bcb3655b4c13820297ca52
Sivabudh Umpudh <sivabudh.u@codium.co>
2020-05-02 14:04:58 +0700

    Code to upload image.
    ...still testing.

--- src/main/java/example/HelloWorldClient.java
+++ src/main/java/example/HelloWorldClient.java
@@ -10,8 +10,21 @@ public class HelloWorldClient {
         try {
             var service = new IWSLim5APserviceLocator();
             var port = service.getIWSLim5APPort();
-            var result = port.txtcompname();
-            System.out.println(result);
+            var txtcompnameResult = port.txtcompname();
+            System.out.println(txtcompnameResult);
+
+            var attachmentPath = HelloWorldClient.class.getResource("/attachment.jpg");
+            System.out.println(attachmentPath);
+
+            var fileDataSource = new javax.activation.FileDataSource(attachmentPath.toString());
+            var dataHandler = new javax.activation.DataHandler(fileDataSource);
+
+            System.out.println("Uploading...");
+
+            var IMGsdResult = port.IMGsd("SendOK", dataHandler);
+            System.out.println(IMGsdResult);
+
+            System.out.println("Finished!");
         } catch (ServiceException | RemoteException ex) {
             ex.printStackTrace();
         }
